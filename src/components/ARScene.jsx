3e82c1abb8f999d8ac95b4a8cfbc56279970fa25
Aklile <akliletilahun@gmail.com>
2024-07-10 13:22:41 +0300

    store

--- src/components/ARScene.jsx
+++ src/components/ARScene.jsx
@@ -1,5 +1,6 @@
 // src/components/ARScene.jsx
 import React, { useEffect } from 'react';
+import 'aframe';
 
 const ARScene = ({ selectedModel }) => {
   useEffect(() => {
@@ -13,18 +14,17 @@ const ARScene = ({ selectedModel }) => {
       const newModel = document.createElement('a-entity');
       newModel.setAttribute('id', 'model');
       newModel.setAttribute('gltf-model', `url(/models/${selectedModel}.glb)`);
-      newModel.setAttribute('position', '0 0 0');
+      newModel.setAttribute('position', '0 0 -3'); // Position the model 3 meters in front of the camera
       newModel.setAttribute('scale', '0.5 0.5 0.5');
       scene.appendChild(newModel);
     }
   }, [selectedModel]);
 
   return (
-    <a-scene embedded arjs>
-      <a-marker preset="hiro">
-        <a-entity id="model"></a-entity>
-      </a-marker>
-      <a-entity camera></a-entity>
+    <a-scene embedded vr-mode-ui="enabled: false" renderer="logarithmicDepthBuffer: true;">
+      <a-entity camera look-controls position="0 1.6 0"></a-entity>
+      <a-light type="ambient" color="#445"></a-light>
+      <a-light type="directional" color="#fff" position="1 1 1"></a-light>
     </a-scene>
   );
 };
