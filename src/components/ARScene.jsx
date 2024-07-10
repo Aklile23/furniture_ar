90ec0b7ab9bbbf64f316977f89d55877f4c999e4
Aklile <akliletilahun@gmail.com>
2024-07-10 13:38:11 +0300

    AR fix

--- src/components/ARScene.jsx
+++ src/components/ARScene.jsx
@@ -1,6 +1,7 @@
 // src/components/ARScene.jsx
 import React, { useEffect } from 'react';
 import 'aframe';
+import 'webxr-polyfill';
 
 const ARScene = ({ selectedModel }) => {
   useEffect(() => {
@@ -14,15 +15,22 @@ const ARScene = ({ selectedModel }) => {
       const newModel = document.createElement('a-entity');
       newModel.setAttribute('id', 'model');
       newModel.setAttribute('gltf-model', `url(/models/${selectedModel}.glb)`);
-      newModel.setAttribute('position', '0 0 -3'); // Position the model 3 meters in front of the camera
+      newModel.setAttribute('position', '0 0 -1'); // Position the model 1 meter in front of the camera
       newModel.setAttribute('scale', '0.5 0.5 0.5');
       scene.appendChild(newModel);
     }
   }, [selectedModel]);
 
   return (
-    <a-scene embedded vr-mode-ui="enabled: false" renderer="logarithmicDepthBuffer: true;">
-      <a-entity camera look-controls position="0 1.6 0"></a-entity>
+    <a-scene
+      vr-mode-ui="enabled: false"
+      renderer="logarithmicDepthBuffer: true;"
+      xrweb="mode: 'ar';"
+      embedded
+    >
+      <a-camera position="0 0 0">
+        <a-cursor></a-cursor>
+      </a-camera>
       <a-light type="ambient" color="#445"></a-light>
       <a-light type="directional" color="#fff" position="1 1 1"></a-light>
     </a-scene>
